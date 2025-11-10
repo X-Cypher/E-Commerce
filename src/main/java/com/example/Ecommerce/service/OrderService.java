9fd7604c011fe2c7e335f7dfefa983e3d7dbcd13
Arjit Gautam <arjitgautamw2015@gmail.com>
2025-11-10 21:21:48 +0530

    Created OrderService

---
+++ src/main/java/com/example/Ecommerce/service/OrderService.java
@@ -0,0 +1,75 @@
+package com.example.Ecommerce.service;
+
+import com.example.Ecommerce.dto.OrderDTO;
+import com.example.Ecommerce.dto.OrderItemDTO;
+import com.example.Ecommerce.entity.Order;
+import com.example.Ecommerce.entity.OrderItem;
+import com.example.Ecommerce.entity.Product;
+import com.example.Ecommerce.entity.User;
+import com.example.Ecommerce.repo.OrderRepo;
+import com.example.Ecommerce.repo.ProductRepo;
+import com.example.Ecommerce.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Autowired
+    private OrderRepo orderRepo;
+
+    // We are returning OrderDTO because we want to display only the relevant data to the user, not the whole order. So orderDto contains only the relevant order details not all
+    public OrderDTO placeOrder(Long userId, Map<Long, Integer> productQuantities) {
+
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new RuntimeException("No user with id " + userId + " found"));
+
+        Order order = new Order(); //create new order
+        order.setUser(user);
+        order.setOrderDate(new Date());
+        order.setStatus("Pending");
+        order.setTotalAmount(productQuantities.values().stream().mapToDouble(i -> i).sum());
+
+        // for order we need order items
+        List<OrderItem> orderItems = new ArrayList<>();
+        List<OrderItemDTO> orderItemsDTO = new ArrayList<>(); //ye isliye bnaya hai kyuki return OrderDTO krna hai and usme OrderItemsDTO chaiye
+
+        for(Map.Entry<Long, Integer> entry: productQuantities.entrySet()){
+
+            Long productId = entry.getKey();
+            Integer quantity = entry.getValue();
+            if(quantity <= 0){
+                throw new RuntimeException("Product Quantity must be greater than 0");
+            }
+
+            OrderItem orderItem = new OrderItem(); //create new order item
+
+            // for order item we need product
+            Product product = productRepo.findById(productId)
+                    .orElseThrow(() -> new RuntimeException("Product with id " + productId + " not found"));
+            orderItem.setProduct(product);
+            orderItem.setQuantity(quantity);
+            orderItem.setOrder(order);
+
+            // add curr order item to order items list
+            orderItems.add(orderItem);
+            orderItemsDTO.add(new OrderItemDTO(product.getName(), product.getPrice(), quantity));
+        }
+
+        order.setOrderItems(orderItems);
+        Order savedOrder = orderRepo.save(order);
+
+        return new OrderDTO(savedOrder.getId(), savedOrder.getTotalAmount(), savedOrder.getStatus(), savedOrder.getOrderDate(), orderItemsDTO);
+    }
+}
